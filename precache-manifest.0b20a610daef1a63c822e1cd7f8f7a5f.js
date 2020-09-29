7958b20dd5625031522d3935b1508b1395294708
Stanyke <Chibuike.stanley50@gmail.com>
2020-09-29 13:20:11 +0100

    Updates

---
+++ precache-manifest.0b20a610daef1a63c822e1cd7f8f7a5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d892b15d3a695ac567f5f1a7d9a05341",
+    "url": "/Book-Directory-System-Frontend/index.html"
+  },
+  {
+    "revision": "cbdcd35c1dd3049c3474",
+    "url": "/Book-Directory-System-Frontend/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "9a7ce84a169c829609dc",
+    "url": "/Book-Directory-System-Frontend/static/css/main.af958183.chunk.css"
+  },
+  {
+    "revision": "cbdcd35c1dd3049c3474",
+    "url": "/Book-Directory-System-Frontend/static/js/2.1fafc0fb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Book-Directory-System-Frontend/static/js/2.1fafc0fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a7ce84a169c829609dc",
+    "url": "/Book-Directory-System-Frontend/static/js/main.7f97b512.chunk.js"
+  },
+  {
+    "revision": "f738849efe8d91ebf1e0",
+    "url": "/Book-Directory-System-Frontend/static/js/runtime-main.b694e8be.js"
+  }
+]);
